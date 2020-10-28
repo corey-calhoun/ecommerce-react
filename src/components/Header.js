5a1cd925d56cf45b1d19adcea09b26f43a39e328
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-27 19:01:06 -0500

    --- Auto-Git Commit ---

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './../assets/css/Header.css'
+function Header() {
+    return (
+        <div className='header'>
+            <h1>Header Component</h1>
+            <img 
+             className="header__logo"
+             src="#"
+             alt=""
+             />/
+        </div>
+    )
+}
+
+export default Header
