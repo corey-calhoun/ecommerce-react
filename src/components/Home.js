efc924fc5ee8fe0523901f633a047a09281f5a70
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-28 15:54:34 -0500

    --- Auto-Git Commit ---

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,29 @@ import '../assets/css/Home.css'
 import Product from './Product'
 
 function Home() {
+
+
     return (
         <div className='home'>
-            <img
-             className="home__image"
-             src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Yzk3OWQ3NDUt/Yzk3OWQ3NDUt-NTAyODZlMmUt-w3000._CB417503089_.jpg" 
-             alt=""
-            />
-            
-            <div className="home__row">
-                <Product />
-                {/* product */}
-                {/* product */}
-                {/* product */}
+            <div className="home__container">
+                <img
+                className="home__image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Yzk3OWQ3NDUt/Yzk3OWQ3NDUt-NTAyODZlMmUt-w3000._CB417503089_.jpg" 
+                alt=""
+                />
+                
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                    
+                    
+                </div>
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */}
+                    {/* product */}
+                </div>
             </div>
-            <div className="home__row">
-                {/* product */}
-                {/* product */}
-                {/* product */}
-            </div>
-            
         </div>
     )
 }
