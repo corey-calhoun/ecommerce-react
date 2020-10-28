4e001fc31f0a187fe7986f53439de817765d0fc1
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-28 15:24:25 -0500

    --- Auto-Git Commit ---

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,18 +5,31 @@ import '../assets/css/Product.css'
 function Product(title, price) {
     return (
         <div className="product">
-            <img 
+            <div className="product__container">
+                <img 
                     className="product__image"
                     src="https://m.media-amazon.com/images/I/51qX1PpW4fL._AC_SY480_.jpg"
                     alt=""
                 />
-            <div className="product__info">
-                <h3>Product Title</h3>
-                <h4>Product Price</h4>
-                <small>$</small>
-                <strong>19.99</strong>
+                <div className="product__info">
+                    <h3>Product Title</h3>
+                    <div className="product__price">
+                        <h4>Product Price</h4>
+                        <small>$</small>
+                        <strong>19.99</strong>
+                    </div>
+                    
+                </div>
+                <Button
+                    className="product__button"
+                    size="small"
+                    variant="contained"
+                    color="secondary"
+                >
+                    Add to Basket 
+                </Button>
             </div>
-            <Button >Add to Basket</Button>
+            
         </div>
     )
 }
