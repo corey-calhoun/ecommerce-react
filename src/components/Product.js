5110bb92537325bcb6ea849b0b4053f2c23ddf7b
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-29 14:46:01 -0500

    --- Auto-Git Commit ---

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,3 @@
-import { Button } from '@material-ui/core'
 import { Star } from '@material-ui/icons'
 import React from 'react'
 import '../assets/css/Product.css'
@@ -25,42 +24,25 @@ function Product({id, title, image, price, rating}) {
 
     return (
         <div className="product">
-            <div className="product__container">
-                <img 
-                    className="product__image"
-                    src={image}
-                    alt=""
-                />
-                <div className="product__info">
-                    <h5>{title}</h5>
-                    <div className="product__price">
-                        <small>$</small>
-                        <strong>{price}</strong>
-                    </div>
-                    {Array(rating)
-                        .fill()
-                        .map((_, i) => (
-                            <Star 
-                             className="product__rating"
-                             size="small"
-                            />
-                        ))}
-                    
+            <img src={image} alt="" />
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                <small>$</small>
+                <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                    <Star />
+                    ))}
                 </div>
-                
-                <Button
-                    className="product__button"
-                    size="small"
-                    variant="contained"
-                    color="secondary"
-                    onClick={addToBasket}
-                >
-                    Add to Basket 
-                </Button>
             </div>
-            
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
-    )
-}
+      );
+    }
+    
 
 export default Product
