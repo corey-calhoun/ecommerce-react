ec6d0149d7c1eec8d6f53902e29e865131af7710
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-29 00:16:28 -0500

    --- Auto-Git Commit ---

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import '../assets/css/Subtotal.css'
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "../reducer";
+import { useHistory } from "react-router-dom";
+
 
 
 function Subtotal() {
+    const history = useHistory();
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
             <CurrencyFormat
